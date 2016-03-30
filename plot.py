d1c082affbfeb3f77e9c0c5ca209042e132adc7d
Basil Schneider <basil.schneider@cern.ch>
2016-03-30 16:41:03 -0500

    Add functionality to give path to log files as positional argument

--- plot.py
+++ plot.py
@@ -3,20 +3,19 @@
 """ Author: Basil Schneider <basil.schneider@cern.ch>
 Get plots from MPA measurements. """
 
+from sys import argv
 from RippleCounter import RippleCounter
 from BunchCrossing import BunchCrossing
 from HitMap import HitMap
 
 if __name__ == '__main__':
 
-    rc = RippleCounter()
+    # Get the path to the logs
+    path_logs = argv[1]
+    # Need timestamp from path, this method is not foolproof!
+    path_timestamp = path_logs[path_logs.find('daqout'):].split('_')[3]
 
-    # Define where to get the logfiles
-    #path_timestamp = '112132256811'
-    #path_logs = '/home/bschneid/MAPSA_Software/daqlogs/'
-    #path_logs+= 'daqout_default_noprocessing_%s_none/' % path_timestamp
-    path_logs = '/home/bschneid/MAPSA_Software/daqlogs/daqout_default_noprocessing_143334709341_thr80_sh5000_nosh100_laserON'
-    path_timestamp = '143334709341'
+    rc = RippleCounter()
 
     # Read in data from raw log file and store it in MPA object
     rc.read_data_raw('%s/log_%s.log_counter' % (path_logs, path_timestamp))
